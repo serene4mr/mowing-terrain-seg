92d9f47250a90ccdcb5bb49c23fe23501fc5db00
zeki-aitech <trungnh.aitech@gmail.com>
2026-01-24 05:43:17 +0000

    Refactor BasePredictor class to enhance model loading and data preparation. Introduced device parameter for GPU support, updated method names for clarity, and implemented data preparation logic for Torch backend. Added placeholders for ONNX and TensorRT support.

--- src/mowing_terrain_seg/inference/predictor.py
+++ src/mowing_terrain_seg/inference/predictor.py
@@ -1,8 +1,13 @@
-from typing import Any, Literal, Union, Sequence
+from collections import defaultdict
+from typing import Any, Union, Sequence
 from abc import ABC, abstractmethod
+from enum import Enum
 
 import numpy as np
 import torch
+
+import mmseg
+from mmengine.dataset import Compose
 
 class Backend(str, Enum):
     TORCH = "torch"
@@ -10,29 +15,88 @@ class Backend(str, Enum):
     TENSORRT = "tensorrt"
 
 class BasePredictor(ABC):
-    def __init__(self, cfg_uri: dict, model_uri: str, backend: Backend):
+    def __init__(self, cfg_uri: str, model_uri: str, backend: Backend, device: str = 'cuda:0'):
         """
         
         """
         
         self.cfg_uri = cfg_uri
-        self.model_uri = model_uri
+        self.model_uri = model_uri # checkpoint path in term of torch backend
         self.backend =  backend
+        self.device = device
         
-    def load_model(self) -> None:
+        self.model = None # placeholder for model
+        self.cfg = None # placeholder for cfg
+        
+    def __call__(self):
+        self._load_model()
+        
+    def _load_model(self) -> None:
         """
         Load model/engine from self.model_uri based on backend
         """
-        from mmengine.config import Config
+        if self.backend == Backend.TORCH:
+            from mmengine.config import Config
+            from mmseg.apis.inference import init_model
+            
+            self.cfg = Config.fromfile(self.cfg_uri)
+            
+            self.model = init_model(
+                config = self.cfg,
+                checkpoint=self.model_uri,
+                device=self.device
+            )
+            
+        if self.backend == Backend.ONNX or Backend.TENSORRT:
+            # TODO: implement later
+            raise NotImplementedError
         
+    
+    def _prepare_data(self, imgs: Union[np.ndarray, Sequence[np.ndarray]]):
+        """
+        """
+        test_pipeline = []
+        if self.backend == Backend.TORCH:
+            
+            test_pipeline = self.model.cfg.test_pipeline
+            for t in test_pipeline:
+                if t.get('type') == 'LoadAnnotations':
+                    test_pipeline.remove(t)
+        
+        if self.backend == Backend.ONNX or Backend.TENSORRT:
+            # test_pipeline = [
+            #     {'type': 'LoadImageFromNDArray'},
+            #     {'keep_ratio': True, 'scale': (1024, 544), 'type': 'Resize'},
+            #     {'type': 'PackSegInputs'}
+            # ]
+            # TODO: implement later
+            raise NotImplementedError
 
-    @abstractmethod
+        is_batch = True
+        if not isinstance(imgs, (list, tuple)):
+            imgs = [imgs]
+            is_batch = False
+
+        pipeline = Compose(test_pipeline)
+
+        data = defaultdict(list)
+        for img in imgs:
+            if isinstance(img, np.ndarray):
+                data_ = dict(img=img)
+            else:
+                data_ = dict(img_path=img)
+            data_ = pipeline(data_)
+            data['inputs'].append(data_['inputs'])
+            data['data_samples'].append(data_['data_samples'])
+
+        return data, is_batch
+        
     def _preprocess(
         self,
-        images: Union[np.ndarray, Sequence[np.ndarray]],
+        imgs: Union[np.ndarray, Sequence[np.ndarray]],
     ) -> Any:
         """Convert raw images (HWC np.ndarray) -> batch input for backend."""
-        ...
+        pass
 
     @abstractmethod
     def _forward(self, batch: Any) -> Any:
@@ -47,19 +111,19 @@ class BasePredictor(ABC):
         ...   
              
     
-    def predict(self, images: Union[np.ndarray, Sequence[np.ndarray]]):
+    def predict(self, imgs: Union[np.ndarray, Sequence[np.ndarray]]):
         """
         Run inference on one or a batch of images
 
         Args:
-            images:
+            imgs:
             - np.ndarray: single image
             - Sequence[np.ndarray]: list/tuple of images
             
         Returns:
             - output for single input, or list of outputs for batch
         """
-        batch = self._preprocess(images)
+        batch = self._preprocess(imgs)
         raw = self._forward(batch)
         outputs = self._postprocess(raw)
         return outputs
@@ -67,8 +131,8 @@ class BasePredictor(ABC):
 
 class SegPredictor(BasePredictor):
     
-    def __init__(self, cfg: dict, model_uri: str, backend: Backend):
-        super().__init__(cfg, model_uri, backend)
+    def __init__(self, cfg_uri: str, model_uri: str, backend: Backend, device: str =' cuda:0'):
+        super().__init__(cfg_uri, model_uri, backend, device)
         
         
         
